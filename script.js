49a677e6a7a59fd28d5363ce26b95665d8573473
Wren <wentarohowell@gmail.com>
2019-11-12 09:35:40 -0500

    This is a prototype that we presented for Nitezeche

--- script.js
+++ script.js
@@ -32,6 +32,16 @@ var zoomer = function (){
  
 }
 
+
+// var nystories = document.querySelector("p").offsetTop;
+// window.onscroll = function() {
+//   if (window.pageYOffset > 0) {
+//  var opac = (window.pageYOffset / nystories);
+//     console.log(opac);
+//   document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/times-square-perspective.jpg) no-repeat";
+//   }
+// }
+
 // window.onload = function (){
 //   var zoomer = function (){
 //       document.getElementById('img-zoomer-box-1')
